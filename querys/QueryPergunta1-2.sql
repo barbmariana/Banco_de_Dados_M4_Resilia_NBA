c6b0ae36f878d13278b1c08d5c0bff04f650ddfd
Mariana Barbosa de Souza <marianasouzaeco@gmail.com>
2023-02-15 10:34:55 -0300

    query1

---
+++ querys/QueryPergunta1-2.sql
@@ -0,0 +1,6 @@
+SELECT ranking.TEAM_ID, ranking.SEASON_ID, ranking.W as VITORIAS, teams.NICKNAME as NOME_TIME from ranking
+inner join teams 
+on ranking.TEAM_ID = teams.TEAM_id
+group by ranking.TEAM_ID
+order by ranking.W asc
+limit 3;
