0d3f5757533ea9b13f135950d28335382081add7
Andrew Deras <andrewderas7@gmail.com>
2023-02-15 22:12:58 -0300

    some updates

--- querys/query4.sql
+++ querys/query4.sql
@@ -1,7 +1,8 @@
 SELECT  teams.NICKNAME AS time ,games.PTS_home AS pontos_em_casa, games.GAME_DATE_EST AS data_do_jogo
 FROM games
 INNER JOIN teams
-on games.TEAM_ID_away = teams.TEAM_ID
-order by games.PTS_home desc 
-LIMIT 10
+ON games.TEAM_ID_away = teams.TEAM_ID
+WHERE games.GAME_DATE_EST LIKE "2019%"
+ORDER BY games.PTS_home DESC 
+LIMIT 5
 ;
